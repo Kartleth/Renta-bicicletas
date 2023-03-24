546ffcd7859ab2de27ce70f7e099058a7653fe8d
Kartleth <Karlaarlethlerma@gmail.com>
2023-03-24 10:12:11 -0700

    Agregar index.html

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for, abort
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+    
+if __name__ == '__main__':
+    app.run(debug=True)
